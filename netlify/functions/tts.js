1a84277c24533f7dbb963bdaf3cf71d3521498f8
agenceproreseaux-max <agence.pro.reseaux@gmail.com>
2026-05-30 22:11:00 +0200

    Update tts.js

--- netlify/functions/tts.js
+++ netlify/functions/tts.js
@@ -2,7 +2,6 @@ exports.handler = async (event) => {
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
-
   return {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
